6cd5c5fd81bd23ca663e594d2c3b6c6e893f03e3
Isaac Mares Meza <maresmezaisaac1@gmail.com>
2023-09-17 12:11:58 -0600

    Agregando una opcion de localizacion para la pagina

--- MiCurso/MiCurso/urls.py
+++ MiCurso/MiCurso/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Home/', include("news.urls")),
+    path('', include("news.urls")),
     path('admin/', admin.site.urls),
 ]
